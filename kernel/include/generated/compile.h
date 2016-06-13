c56a6a39dab4dcabab32ce360c85fbdc33bd2295
I1PABIJJA <gamehostplay@gmail.com>
2016-06-13 10:09:02 +0500

    fix

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sun Jun 12 19:17:22 YEKT 2016"
+#define UTS_VERSION "#3 SMP PREEMPT Mon Jun 13 09:07:09 YEKT 2016"
 #define LINUX_COMPILE_BY "Android-build"
 #define LINUX_COMPILE_HOST "HP-ProLiant-DL580"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
